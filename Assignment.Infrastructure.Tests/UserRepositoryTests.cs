1d4b5aa7fa3ebed751ba7217416c9bf1526eef30
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2022-10-04 13:19:06 +0100

    User is up and running

--- Assignment.Infrastructure.Tests/UserRepositoryTests.cs
+++ Assignment.Infrastructure.Tests/UserRepositoryTests.cs
@@ -1,5 +1,68 @@
 namespace Assignment.Infrastructure.Tests;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Assignment.Core;
 
-public class UserRepositoryTests
+using Assignment.Infrastructure;
+
+public class UserRepositoryTest
 {
+    private readonly KanbanContext _context;
+    private readonly UserRepository _repository;
+    public UserRepositoryTest()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<KanbanContext>();
+        builder.UseSqlite(connection);
+        var context = new KanbanContext(builder.Options);
+        context.Database.EnsureCreated();
+        var tasks1 = new WorkItem { Id = 1, Title = "HewwoTwere", AssignedTo = null, State = State.New, Tags = new List<Tag> { } };
+        var tag1 = new Tag { Id = 1, Name = "HelloThere", WorkItems = new List<Assignment.Infrastructure.WorkItem> { } };
+        var user1 = new User { Id = 1, Name = "ObiWan", Email = "DankRepublic@corusant.com", WorkItems = new List<Assignment.Infrastructure.WorkItem> { } };
+        tasks1.Tags.Add(tag1);
+        user1.WorkItems.Add(tasks1);
+        tag1.WorkItems.Add(tasks1);
+
+        context.Tags.Add(tag1);
+        context.WorkItems.Add(tasks1);
+        context.Users.Add(user1);
+        context.SaveChanges();
+
+        _context = context;
+        _repository = new UserRepository(_context);
+    }
+
+    [Fact]
+    public void Existing_User_Can_Be_Deleted_Using_The_Force()
+    {
+        var response = _repository.Delete(1, true);
+
+        response.Should().Be(Response.Deleted);
+
+        var entity = _context.Users.Find(1);
+
+        entity.Should().BeNull();
+    }
+
+    [Fact]
+    public void Existing_User_Can_Not_Be_Deleted_Without_Using_The_Force()
+    {
+        var response = _repository.Delete(1, false);
+
+        response.Should().Be(Response.Conflict);
+
+        var entity = _context.Users.Find(1);
+
+        entity.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void User_Which_Exists_Cannot_Be_Created()
+    {
+        var (response, id) = _repository.Create(new UserCreateDTO("ObiWan", "DankRepublic@corusant.com"));
+
+        response.Should().Be(Response.Conflict);
+    }
+
 }
--- Assignment.Infrastructure/UserRepository.cs
+++ Assignment.Infrastructure/UserRepository.cs
@@ -1,5 +1,103 @@
+using System.Xml.Linq;
 namespace Assignment.Infrastructure;
-
-public class UserRepository
+using Assignment.Core;
+using Assignment.Infrastructure;
+public class UserRepository : IUserRepository
 {
+     private readonly KanbanContext _context;
+        public UserRepository(KanbanContext context)
+    {
+        _context = context;
+    }
+
+    public (Response Response, int UserId) Create(UserCreateDTO user)
+    {
+        var entity = _context.Users.FirstOrDefault(u => u.Email == user.Email);
+        Response response;
+
+        if (entity is null)
+        {
+            entity = new User{Id = 1, Email = user.Email};
+
+            _context.Users.Add(entity);
+            _context.SaveChanges();
+
+            response = Response.Created;
+        }
+        else
+        {
+            response = Response.Conflict;
+        }
+
+        return (response, entity.Id);
+
+    }
+
+    public IReadOnlyCollection<UserDTO> Read()
+    {
+        var tags = from u in _context.Users
+                     orderby u.Name
+                     select new UserDTO(u.Id, u.Name!, u.Email);
+
+        return tags.ToArray();
+    }
+
+    public UserDTO Find(int userId)
+    {
+        var tags = from u in _context.Users
+                    where u.Id == userId
+                    select new UserDTO(u.Id, u.Name!, u.Email);
+
+        return tags.FirstOrDefault()!;
+    }
+
+    public Response Update(UserUpdateDTO user)
+    {
+        var entity = _context.Users.Find(user.Id);
+        Response response;
+
+        if (entity is null)
+        {
+            response = Response.NotFound;
+        }
+        else if (_context.Users.FirstOrDefault(u => u.Id != user.Id && u.Email == user.Email) != null)
+        {
+            response = Response.Conflict;
+        }
+        else
+        {
+            entity.Name = user.Name;
+            _context.SaveChanges();
+            response = Response.Updated;
+        }
+
+        return response;
+
+    }
+
+    public Response Delete(int userId, bool force = false)
+    {
+        var user = (from u in _context.Users
+                    where u.Id == userId
+                    select u).FirstOrDefault();
+        Response response;
+        if (user is null)
+        {
+            response = Response.NotFound;
+        }
+        else if (user.WorkItems!.Any() && !force)
+        {
+            response = Response.Conflict;
+        }
+        else
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            response = Response.Deleted;
+        }
+
+        return response;
+    }
+
 }
